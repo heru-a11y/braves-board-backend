383277d3e4b45c34231843a2cb00671f58fab017
herupurnomoht <heru.purnomoht@gmail.com>
2026-04-15 09:53:36 +0700

    refactor: rename files in schemas

--- app/schemas/column_schemas.py
+++ app/schemas/column_schemas.py
@@ -1,17 +1,21 @@
 import uuid
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, ConfigDict
+
 
 class ColumnBase(BaseModel):
     title: str
 
+
 class ColumnCreate(ColumnBase):
     board_id: uuid.UUID
+
 
 class ColumnUpdate(BaseModel):
     title: Optional[str] = None
     position: Optional[int] = None
+
 
 class ColumnResponse(ColumnBase):
     id: uuid.UUID
@@ -22,3 +26,13 @@ class ColumnResponse(ColumnBase):
     deleted_at: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class ColumnSingleResponse(BaseModel):
+    message: str
+    data: ColumnResponse
+
+
+class ColumnListResponse(BaseModel):
+    message: str
+    data: List[ColumnResponse]
