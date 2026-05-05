eb6fc5138d6233838ed08e90838395b251fe2ce9
herupurnomoht <heru.purnomoht@gmail.com>
2026-05-05 13:40:00 +0700

    restrucutre folder

--- src/backend/app/api/auth/views.py
+++ src/backend/app/api/auth/views.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends, Response, Cookie, Query
 from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from jose import jwt
+from fastapi import Query
+from fastapi.responses import RedirectResponse
 
 from app.settings import settings
 from app.connections.postgres import get_db
@@ -34,42 +36,34 @@ from fastapi.responses import RedirectResponse
 
 @router.get("/google/callback")
 async def google_callback(
-    response: Response,
     code: str = Query(...),
     db: AsyncSession = Depends(get_db),
 ):
     user_repo = UserRepository(db)
 
     try:
-        print("CODE:", code)
-
         result = await AuthUseCase.handle_google_callback(code, user_repo)
 
-        print("SUCCESS LOGIN:", result)
+        redirect_response = RedirectResponse(
+            url=f"http://localhost:5173/dashboard?access_token={result['access_token']}"
+        )
 
-        response.set_cookie(
+        redirect_response.set_cookie(
             key="refresh_token",
             value=result["refresh_token"],
             httponly=True,
-            secure=False,  # dev
+            secure=False,
             samesite="lax",
             path="/",
             max_age=604800,
         )
 
-        return RedirectResponse(
-            url=f"http://localhost:5173/dashboard?access_token={result['access_token']}"
-        )
+        return redirect_response
 
-    except Exception as e:
-        import traceback
-        print(" ERROR GOOGLE:", str(e))
-        traceback.print_exc()
-
+    except Exception:
         return RedirectResponse(
-            url=f"http://localhost:5173/?error=google_auth_failed"
+            url="http://localhost:5173/?error=google_auth_failed"
         )
-    # return RedirectResponse(url=redirect_url)
 
 @router.get("/me", response_model=StandardResponse[CurrentUserData])
 async def get_current_user_profile(current_user: User = Depends(get_current_user)):
